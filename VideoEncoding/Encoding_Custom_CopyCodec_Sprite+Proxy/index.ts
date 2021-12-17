ceb6ce789895b57ce1ccd987851f81181434aaa5
John Deutscher <johndeu@microsoft.com>
2021-12-17 11:46:40 -0800

    updates to sample

--- VideoEncoding/Encoding_Custom_CopyCodec_Sprite+Proxy/index.ts
+++ VideoEncoding/Encoding_Custom_CopyCodec_Sprite+Proxy/index.ts
@@ -84,7 +84,7 @@ const setTimeoutPromise = util.promisify(setTimeout);
 
 // Args
 const outputFolder: string = "./Output";
-const namePrefix: string = "encode_copycodec+sprite+proxy";
+const namePrefix: string = "encode_copycodec_sprite_proxy";
 let inputExtension: string;
 let blobName: string;
 
@@ -94,7 +94,7 @@ let blobName: string;
 export async function main() {
 
     // These are the names used for creating and finding your transforms
-    const transformName = "CopyCodecWithSpriteAndProxy";
+    const transformName = "CopyCodecWithSpriteAndProxyCustom";
 
     mediaServicesClient = new AzureMediaServices(credential, subscriptionId);
 
@@ -107,24 +107,22 @@ export async function main() {
             preset: factory.createBuiltInStandardEncoderPreset({
                 presetName: "SaasSourceAligned360pOnly" // There are some undocumented magical presets in our toolbox that do fun stuff - this one is going to copy the codecs from the source and also generate a 360p proxy file.
                 
-                // Other magical presets to play around with...
+                // Other magical presets to play around with, that might (or might not) work for your source video content...
                 // "SaasCopyCodec" - this just copies the source video and audio into an MP4 ready for streaming.  The source has to be H264 and AAC with CBR encoding and no B frames typically. 
                 // "SaasProxyCopyCodec" - this copies the source video and audio into an MP4 ready for streaming and generates a proxy file.   The source has to be H264 and AAC with CBR encoding and no B frames typically. 
-                // "SaasSourceAligned360pOnly" - same as above, but generates a single 360P proxy layer that is aligned in GOP to the source file. Useful for "backfilling" a proxy on a pre-encoded file uploaded.  
-                // "SaasSourceAligned540pOnly"-  generates a single 540P proxy layer that is aligned in GOP to the source file. Useful for "backfilling" a proxy on a pre-encoded file uploaded. 
-                // "SaasSourceAligned540p" - generates an adaptive set of 540P and 360P that is aligned to the source file. used for "backfilling" a pre-encoded or uploaded source file in an output asset for better streaming. 
-                // "SaasSourceAligned360p" - generates an adaptive set of 360P and 180P that is aligned to the source file. used for "backfilling" a pre-encoded or uploaded source file in an output asset for better streaming. 
+                // "SaasSourceAligned360pOnly" - same as above, but generates a single 360P proxy layer that is aligned in GOP to the source file. Useful for "back filling" a proxy on a pre-encoded file uploaded.  
+                // "SaasSourceAligned540pOnly"-  generates a single 540P proxy layer that is aligned in GOP to the source file. Useful for "back filling" a proxy on a pre-encoded file uploaded. 
+                // "SaasSourceAligned540p" - generates an adaptive set of 540P and 360P that is aligned to the source file. used for "back filling" a pre-encoded or uploaded source file in an output asset for better streaming. 
+                // "SaasSourceAligned360p" - generates an adaptive set of 360P and 180P that is aligned to the source file. used for "back filling" a pre-encoded or uploaded source file in an output asset for better streaming. 
             })
         },
         {
         // uses the Standard Encoder Preset to generate copy the source audio and video to an output track, and generate a proxy and a sprite
         preset: factory.createStandardEncoderPreset({
             codecs: [
-                factory.createCopyVideo({  // this part of the sample is a custom copy codec - copies the video track from the source to the output MP4 file
-                    label: "sourcevideo"
+                factory.createCopyVideo({  // this part of the sample is a custom copy codec - It will copy the source video track directly to the output MP4 file
                 }),
                 factory.createCopyAudio({ // this part of the sample is a custom copy codec - copies the audio track from the source to the output MP4 file
-                    label: "sourceaudio"
                 }),
                 factory.createJpgImage({
                     // Also generate a set of thumbnails in one Jpg file (thumbnail sprite)
@@ -147,7 +145,7 @@ export async function main() {
                 // Note that since you have multiple H264Layers defined above, you have to use a macro that produces unique names per H264Layer
                 // Either {Label} or {Bitrate} should suffice
                 factory.createMp4Format({
-                    filenamePattern: "CopyCodec-{Basename}-{Label}-{Bitrate}{Extension}"
+                    filenamePattern: "CopyCodec-{Basename}{Extension}"
                 }),
                 factory.createJpgFormat({
                     filenamePattern: "sprite-{Basename}-{Index}{Extension}"
